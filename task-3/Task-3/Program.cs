202f3642a2c4a39d425dcc1889dadf2cedc0893b
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2024-08-11 18:27:07 +0300

    Completed Task-3.

---
+++ task-3/Task-3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_3
+{
+    using System.Globalization;
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите предложение: ");
+            string str = Console.ReadLine();
+            if (str == "")
+            {
+                Console.WriteLine("Ошибка: пустая строка!");
+                Environment.Exit(0);
+            }
+            string[] words = str.Split(" ");
+            Array.Sort(words);
+            foreach (string word in words)
+            {
+                TextInfo textInfo = new CultureInfo("RU", false).TextInfo;
+                Console.WriteLine(textInfo.ToTitleCase(word));
+            }
+            Console.WriteLine($"Количество слов: {words.Length}");
+        }
+    }
+}
