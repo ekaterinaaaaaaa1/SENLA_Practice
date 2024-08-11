5b9244cf6df1937ebdf0d10783720ba463a64d68
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2024-08-11 20:27:34 +0300

    Completed Task-4.

---
+++ task-4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine().ToLower();
+            if (text == null)
+            {
+                Console.WriteLine("Ошибка: пустая строка!");
+                Environment.Exit(0);
+            }
+            Console.Write("Введите слово: ");
+            string word = Console.ReadLine().ToLower();
+            if (word == null)
+            {
+                Console.WriteLine("Ошибка: пустая строка!");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Слово употребляется в тексте {0} раз(-а)", text.Replace(word, "*").Count(x => x == '*'));
+        }
+    }
+}
