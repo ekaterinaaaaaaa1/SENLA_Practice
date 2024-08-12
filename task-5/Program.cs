2f876d917f65346dd3884ca5f797e0a47dad19f6
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2024-08-12 18:37:55 +0300

    Completed Task-5.

---
+++ task-5/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task_5
+{
+    class Program
+    {
+        const int MAX_VALUE = 100;
+        
+        public static bool isPalindrome(int number)
+        {
+            if (number == reverseNumber(number))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static int reverseNumber(int number)
+        {
+            char[] charArray = number.ToString().ToCharArray();
+            Array.Reverse(charArray);
+
+            return Convert.ToInt32(new string(charArray));
+        }
+
+        static void Main()
+        {
+            Console.Write($"Введите целое число от 0 до {MAX_VALUE}: ");
+            if (!int.TryParse(Console.ReadLine(), out int N))
+            {
+                Console.WriteLine("Ошибка: неверный тип данных!");
+                Environment.Exit(0);
+            }
+            if ((N < 0) || (N > MAX_VALUE))
+            {
+                Console.WriteLine("Ошибка: введённое число вне допустимых значений!");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine($"Числа-палиндромы от 0 до {N}:");
+            for (int i = 0; i <= N; i++)
+            {
+                if (isPalindrome(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
