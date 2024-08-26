9831743ef129100ac49ab3fdc3cf8fc32bd360f1
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2024-08-26 12:58:49 +0300

    Added descriptions.

--- task-3/WordsCounter/StringLengthComparer.cs
+++ task-3/WordsCounter/StringLengthComparer.cs
@@ -1,7 +1,17 @@
 ﻿namespace WordsCounter
 {
+    /// <summary>
+    /// Contains method for comparing strings by lengths.
+    /// </summary>
     public class StringLengthComparer : IComparer<string>
     {
+        /// <summary>
+        /// Compares two strings by length.
+        /// </summary>
+        /// <param name="str1">The first string to compare.</param>
+        /// <param name="str2">The second string to compare.</param>
+        /// <returns>A positive integer if the first string length is longer, a negative integer if the second string length is longer, zero if the lengths are equal.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public int Compare(string? str1, string? str2)
         {
             if (str1 == null || str2 == null)
