98ca1d50440491e205f53e56eb2d811534d1e93a
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2024-09-08 16:21:58 +0300

    Complete Task-6.

---
+++ task-6/Backpack.cs
@@ -0,0 +1,17 @@
+﻿namespace task_6
+{
+    public class Backpack
+    {
+        private Item[] _items;
+        private int _value;
+
+        public Backpack(Item[] items, int value)
+        {
+            _items = items;
+            _value = value;
+        }
+
+        public Item[] Items { get { return _items; } }
+        public int Value { get { return _value; } }
+    }
+}
---
+++ task-6/Item.cs
@@ -0,0 +1,42 @@
+﻿namespace task_6
+{
+    public class Item
+    {
+        private string _name;
+        private int _volume;
+        private int _value;
+
+        public Item(string name, int volume, int value)
+        {
+            _name = name;
+            _volume = volume;
+            _value = value;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Volume
+        {
+            get { return _volume; }
+            set
+            {
+                if (value > 0)
+                _volume = value;
+            }
+        }
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value > 0)
+                _value = value;
+            }
+        }
+    }
+}
