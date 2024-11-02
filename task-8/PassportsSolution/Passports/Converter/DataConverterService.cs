1c7478ba733f6c8fa2d4726dd89b6dc3db54dd22
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2024-11-02 15:03:40 +0300

    Added Copy().

--- task-8/PassportsSolution/Passports/Converter/DataConverterService.cs
+++ task-8/PassportsSolution/Passports/Converter/DataConverterService.cs
@@ -7,46 +7,14 @@ namespace Passports.Converter
 {
     public class DataConverterService(IServiceScopeFactory serviceScopeFactory, IConfiguration configuration) : BackgroundService
     {
-        private string _path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Converter", "data1", "Data1.csv");
-
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             using var scope = serviceScopeFactory.CreateScope();
-            using var context = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
-            
-            await context.Database.EnsureCreatedAsync(stoppingToken);
+            var context = scope.ServiceProvider.GetRequiredService<PostgresDBService>();
 
-            using StreamReader streamReader = new StreamReader(_path);
-            Debug.WriteLine("ExecuteAsync is working");
-            Console.WriteLine("ExecuteAsync is working");
+            // await context.Database.EnsureCreatedAsync(stoppingToken);
 
-            string? line = streamReader.ReadLine();
-            while ((line = streamReader.ReadLine()) != null)
-            {
-                Passport? passport = CsvParserService.Parse(line);
-
-                if (passport != null)
-                {
-                    Passport? existingPassport = context.InactivePassports.Find(passport.Series, passport.Number);
-                    if (existingPassport != null)
-                    {
-                        existingPassport.IsActive = false;
-                        context.InactivePassports.Update(existingPassport);
-                    }
-                    else
-                    {
-                        context.InactivePassports.Add(passport);
-                    }
-                    context.SaveChanges();
-                }
-            }
-
-            //context.SaveChanges();
-
-            /*while (!stoppingToken.IsCancellationRequested)
-            {
-                await Task.Delay(200, stoppingToken);
-            }*/
+            context.Copy();
 
             await Task.CompletedTask;
         }
--- task-8/PassportsSolution/Passports/Services/PostgresDBService.cs
+++ task-8/PassportsSolution/Passports/Services/PostgresDBService.cs
@@ -8,6 +8,8 @@ namespace Passports.Services
     {
         private readonly ApplicationContext _context;
 
+        private string _path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Converter", "data1", "Data1.csv");
+
         public PostgresDBService(ApplicationContext context)
         {
             _context = context;
@@ -16,6 +18,32 @@ namespace Passports.Services
         public Passport? GetPassport(short series, int number)
         {
             return _context.InactivePassports.Find(series, number);
+        }
+
+        public void Copy()
+        {
+            using StreamReader streamReader = new StreamReader(_path);
+
+            string? line = streamReader.ReadLine();
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                Passport? passport = CsvParserService.Parse(line);
+
+                if (passport != null)
+                {
+                    Passport? existingPassport = _context.InactivePassports.Find(passport.Series, passport.Number);
+                    if (existingPassport != null)
+                    {
+                        existingPassport.IsActive = false;
+                    }
+                    else
+                    {
+                        _context.InactivePassports.Add(passport);
+                    }
+                }
+            }
+
+            _context.SaveChanges();
         }
     }
 }
