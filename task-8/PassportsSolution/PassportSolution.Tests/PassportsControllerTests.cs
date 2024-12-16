59465c4c1d8444263ad7493699bb814e55f6789c
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2024-12-16 15:58:34 +0300

    Changes Tests.

--- task-8/PassportsSolution/PassportSolution.Tests/PassportsControllerTests.cs
+++ task-8/PassportsSolution/PassportSolution.Tests/PassportsControllerTests.cs
@@ -22,16 +22,62 @@ namespace PassportSolution.Tests
         }
 
         [Fact]
-        public void GetUssrPassportEmptyEnterReturnsBadRequest()
+        public void GetPassportInvalidSeriesReturnsNotFound()
         {
             var mock = new Mock<IDBService>();
 
-            string? series = null;
-            string? number = null;
+            string? series = "ABC-до";
+            string? number = "147853";
 
             var controller = new PassportsController(mock.Object);
 
             var result = controller.GetPassport(series, number);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void GetPassportInvalidNumberReturnsNotFound()
+        {
+            var mock = new Mock<IDBService>();
+
+            string? series = "XX-до";
+            string? number = "1473";
+
+            var controller = new PassportsController(mock.Object);
+
+            var result = controller.GetPassport(series, number);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void GetPassportHistoryEmptySeriesEnterReturnsBadRequest()
+        {
+            var mock = new Mock<IDBService>();
+
+            string? series = null;
+            string? number = "147853";
+
+            var controller = new PassportsController(mock.Object);
+
+            var result = controller.GetPassportHistory(series, number);
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void GetPassportsHistoriesByDateEmptyStartDayEnterReturnsBadRequest()
+        {
+            var mock = new Mock<IDBService>();
+
+            int? startYear = 2024;
+            int? startMonth = 9;
+            int? startDay = null;
+            int? endYear = 2024;
+            int? endMonth = 12;
+            int? endDay = 15;
+
+            var controller = new PassportsController(mock.Object);
+
+            var result = controller.GetPassportsHistoriesByDate(startYear, startMonth, startDay, endYear, endMonth, endDay);
             Assert.IsType<BadRequestResult>(result);
         }
     }
